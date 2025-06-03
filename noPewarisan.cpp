d9de5b3da83b84e2f284882b16fa654a26f616c2
ilham101106 <pesatoffical321@gmail.com>
2025-06-03 18:53:12 +0700

    membuat noPewarisan

---
+++ noPewarisan.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+class baseClass {
+public: 
+    virtual void perkenalan(){
+        cout<<"Hallo saya Function dari base class";
+    }
+};
+
+class deviredClass : public baseClass {
+public:
+    void perkenalan(){
+        cout<<"Hallo saya Function dari devired Class";
+    }
+};
+
+int main(){
+    deviredClass a;
+    a.perkenalan();
+
+    return 0;
+}
